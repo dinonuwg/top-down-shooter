7f7fff6b2278b496bd97303b7b6495e8510fc657
Piggii16 <ghyonkunk@gmail.com>
2025-01-04 16:46:04 +0530

    enemy script

--- Top Down Shooter/Assets/enemyScript.cs
+++ Top Down Shooter/Assets/enemyScript.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class enemyScript : MonoBehaviour
 {
+    [SerializeField] private float collisionDmg = 5f;
+    [SerializeField] private float knockback = 5f; 
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
